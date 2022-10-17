f7d1c0e94837f2f4fcfb33f53301eded50ae3aa7
uruems87 <kingforlife20@gmail.com>
2022-10-17 01:16:59 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- *
+ * 
  * return: Always 0 (success)
 */
-  
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
